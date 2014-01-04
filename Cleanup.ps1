1053f4bc916a70b55aacb49b50f14c6ae2c4aae7
enigma0x3 <matthew@webnelson.net>
2014-01-04 15:34:38 -0500

    Update Cleanup.ps1

--- Cleanup.ps1
+++ Cleanup.ps1
@@ -1,3 +1,8 @@
+$Path = "C:\Temp\persist.vbs"
+If(Test-Path -Path $Path){
+	Remove-Item($Path)
+}
+
 $RegKeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 $Value = (Get-ItemProperty $RegKeyPath).Persist -eq $null
 If ($Value -eq $False) {Remove-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Run -Name Persist}
